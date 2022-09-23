5624f48a91f929c4f857fb3689c16104cabc3d9a
Suriya Murugesh <suriyamu@student.tce.edu>
2022-09-23 11:44:37 +0530

    First commit

---
+++ JS/preQuizQues_Expt1.js
@@ -0,0 +1,40 @@
+
+const quiz =[
+{
+	q:'What is the basic component of power electronic circuits?',
+	options:['Diode','SCR','Resistor','Capacitor'],
+	answer:1
+},
+{
+	q:'What is required for turning on an SCR?',
+	options:['Gate Pulse','Reverse bias','Forward Bias','Option (A) and (C)'],
+	answer:3
+},
+{
+	q:'The potential barrier voltage of silicon is',
+	options:['0.5V','0.3V','0.7V','1V'],
+	answer:2
+},
+{
+	q:'Application of full converter',
+	options:['Speed Control of drives','Reducing harmonics','Improving power factor','all the above'],
+	answer:0
+},
+{
+	q:'What is the purpose of SCR in power Electronics?',
+	options:['Oscillator','Amplifier','Rectifier','Switching'],
+	answer:3
+},
+// {
+// 	q:"Forward biasing of p-n junction offers infinite resistance.",
+// 	options:['True','False','No option','No option'],
+// 	answer:1
+// },
+{
+	q:"When a junction diode is reverse biased, what causes current across the junction?",
+    options:['Diffusion of charges','Nature of material','Drift of charges','Both drift and diffusion of charges'],
+    answer:2 
+}
+]
+
+

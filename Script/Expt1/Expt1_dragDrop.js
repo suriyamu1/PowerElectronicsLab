9a396e0253604b01a084a4d6ccaec551dae5a670
Manojkumar273 <manoj2002tce@gmail.com>
2022-11-01 13:16:44 +0530

    draganddrop feature included

--- Script/Expt1/Expt1_dragDrop.js
+++ Script/Expt1/Expt1_dragDrop.js
@@ -1,19 +1,46 @@
+const scr = document.getElementById("scr");
+const diode = document.getElementById("diode");
+const rload = document.getElementById("rload");
+const rl_load = document.getElementById("rl-load");
+const mca = document.getElementById("mca");
+const mcv = document.getElementById("mcv");
+const mia = document.getElementById("mia");
+const miv = document.getElementById("miv");
 
 let dragId;
 
 function allowDrop(ev) {
   ev.preventDefault();
+} 
+scr.ondragstart = (ev)=> {
+  dragId =ev.target.id;
 }
-  
-start1= (ev)=> {
-  ev.preventDefault();
-  console.log("Hi");
+diode.ondragstart = (ev)=> {
+  dragId =ev.target.id;
+}
+rload.ondragstart = (ev)=> {
+  dragId =ev.target.id;
+}
+rl_load.ondragstart = (ev)=> {
+  dragId =ev.target.id;
+}
+mca.ondragstart = (ev)=> {
+  dragId =ev.target.id;
+}
+mcv.ondragstart = (ev)=> {
+  dragId =ev.target.id;
+}
+mia.ondragstart = (ev)=> {
+  dragId =ev.target.id;
+}
+miv.ondragstart = (ev)=> {
+  dragId =ev.target.id;
 }
   
 function drop(ev) {
   ev.preventDefault();
   var img = document.createElement('img');
-  img.src = "../../Assets/Images/dragAndDropImages/Expt1/scr.PNG";
+  img.src = `images/${dragId}.PNG`;
   ev.target.style.border = "none";
   ev.target.appendChild(img);
 }

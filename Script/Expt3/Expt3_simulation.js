ba5b2d4f4b4ed2118cb9bb2a546b2e1c8239466f
Manojkumar273 <manoj2002tce@gmail.com>
2022-12-17 23:13:35 +0530

    expt 3 and 4 graph

---
+++ Script/Expt3/Expt3_simulation.js
@@ -0,0 +1,215 @@
+const output = document.getElementById("output");
+const simulation = document.getElementById("simulation");
+const avgDisplay = document.getElementById("avgDisplay");
+const rmsDisplay = document.getElementById("rmsDisplay");
+const voltageDisplay = document.getElementById("voltageDisplay");
+
+let chart1=null;
+let chart2=null;
+let chart3=null;
+
+const data={
+    referenceValue:12,
+    voltage:30
+}
+
+reference.oninput=()=>{
+    data.referenceValue=Number(reference.value);
+    voltageDisplay.innerHTML= data.referenceValue+" V";
+    showGraph();
+}
+
+function showGraph(){
+    let triangleValues=[];
+    let referenceValues=[];
+    let comparatorValues=[];
+    let outputValues=[];
+    let labelValue=[];
+    let theta=0;
+
+    for(let i=0; i<=100; i++){
+        labelValue.push(i+"");
+    }
+
+    for(let i=0;i<=100;i++){
+        theta=theta%20;
+        if(theta<=5){
+            triangleValues.push(2.4*theta);
+        }else if(theta>5 && theta<=15){
+            triangleValues.push(24-(2.4*theta));
+        }else if(theta>15 && theta<=20){
+            triangleValues.push((2.4*theta)-48);
+        }
+        theta+=1;
+        referenceValues.push(data.referenceValue);
+    }
+
+    for(let i=0;i<=100;i++){
+        if(triangleValues[i]<=data.referenceValue){
+            comparatorValues.push(12);
+            outputValues.push(data.voltage);
+        }
+        else{
+            comparatorValues.push(0);
+            outputValues.push(0);
+        }
+    }
+
+    const ctx1 = document.getElementById("waveform1");
+    if(chart1!==null) chart1.destroy();
+    chart1 = new Chart(ctx1, {
+        type: 'line',
+        data: {
+            datasets:[{
+                label:"Carrier Signal",
+                lineTension:0.17,
+                backgroundColor:"rgba(255, 121, 121,1.0)",
+                borderColor:"#2E0249",
+                data:triangleValues
+            },{
+                label:"Reference Signal",
+                backgroundColor:"rgba(255, 121, 121,1.0)",
+                borderColor:"#2E0225",
+                data:referenceValues
+            }],
+            labels:labelValue,
+        },
+        options: {
+            elements:{
+                point:{
+                    radius:0
+                }
+            },
+            scales:{
+                y: {
+                    suggestedMin: -15,
+                    suggestedMax: 15
+                },
+                yAxes:{
+                    title:{
+                        display:true,
+                        text:"Voltage(V)",
+                        font: {
+                            size: 15
+                        }
+                    }
+                },
+                xAxes:{
+                    title:{
+                        display:true,
+                        text:"time(milliseconds)",
+                        font: {
+                            size: 15
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+    const ctx2 = document.getElementById("waveform2");
+    if(chart2!==null) chart2.destroy();
+    chart2 = new Chart(ctx2, {
+        type: 'line',
+        data: {
+            datasets:[{
+                label:"Comparator Output",
+                lineTension:0.17,
+                backgroundColor:"rgba(255, 121, 121,1.0)",
+                borderColor:"#2E0249",
+                data:comparatorValues
+            }],
+            labels:labelValue,
+        },
+        options: {
+            elements:{
+                point:{
+                    radius:0
+                }
+            },
+            scales:{
+                y: {
+                    suggestedMin: -15,
+                    suggestedMax: 15
+                },
+                yAxes:{
+                    title:{
+                        display:true,
+                        text:"Voltage(V)",
+                        font: {
+                            size: 15
+                        }
+                    }
+                },
+                xAxes:{
+                    title:{
+                        display:true,
+                        text:"time(milliseconds)",
+                        font: {
+                            size: 15
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+    const ctx3 = document.getElementById("waveform3");
+    if(chart3!==null) chart3.destroy();
+    chart3 = new Chart(ctx3, {
+        type: 'line',
+        data: {
+            datasets:[{
+                label:"DC Chopper Output",
+                lineTension:0.17,
+                backgroundColor:"rgba(255, 121, 121,1.0)",
+                borderColor:"#2E0249",
+                data:outputValues
+            }],
+            labels:labelValue,
+        },
+        options: {
+            elements:{
+                point:{
+                    radius:0
+                }
+            },
+            scales:{
+                y: {
+                    suggestedMin: -15,
+                    suggestedMax: 15
+                },
+                yAxes:{
+                    title:{
+                        display:true,
+                        text:"Voltage(V)",
+                        font: {
+                            size: 15
+                        }
+                    }
+                },
+                xAxes:{
+                    title:{
+                        display:true,
+                        text:"time(milliseconds)",
+                        font: {
+                            size: 15
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+    output.classList.remove("hide");
+    simulation.classList.add("hide");
+}
+
+function showCircuit(){
+    chart1.destroy();
+    chart2.destroy();
+    chart3.destroy();
+    output.classList.add("hide");
+    simulation.classList.remove("hide");
+}
+
---
+++ Script/Expt4/Expt4_simulation.js
@@ -0,0 +1,124 @@
+const output = document.getElementById("output");
+const simulation = document.getElementById("simulation");
+const alpha1 = document.getElementById("alpha1");
+const alpha2 = document.getElementById("alpha2");
+const angleDisplay1 = document.getElementById("angleDisplay1");
+const angleDisplay2 = document.getElementById("angleDisplay2");
+const avgDisplay = document.getElementById("avgDisplay");
+const rmsDisplay = document.getElementById("rmsDisplay");
+
+const data ={
+    firingangle1:0,
+    firingangle2:0,
+    voltage:230
+}
+
+function calculateCos(degrees) {
+    var radians = (Math.PI / 180) * degrees;
+    return Math.cos(radians);
+}
+
+function calculateSine(degrees) {
+    var radians = (Math.PI / 180) * degrees;
+    return Math.sin(radians);
+}
+
+alpha1.oninput=()=>{
+    data.firingangle1=Number(alpha1.value);
+    angleDisplay1.innerHTML=data.firingangle1+"&#176;";
+    // let avg = ((2*data.voltage)/Math.PI)*(calculateCos(angle));
+    // avgDisplay.innerHTML=avg.toPrecision(5)+" V";
+    // // console.log(avg);
+    // let rms = (data.voltage/Math.sqrt(2*Math.PI))*Math.sqrt(Math.PI-( (Math.PI / 180) * angle)+(calculateSine(angle)/2));
+    // rmsDisplay.innerHTML=rms.toPrecision(5)+" V";
+    // // console.log(rms);
+    showGraph();
+}
+
+alpha2.oninput=()=>{
+    data.firingangle2=180+Number(alpha2.value);
+    angleDisplay2.innerHTML= data.firingangle2+"&#176;";
+    showGraph();
+}
+
+let chart=null;
+
+function showGraph(){
+    let sineValues=[];
+    let labelValue=[];
+    let theta=0;
+
+    for(let i=0; i<=720; i++){
+        labelValue.push(i+"");
+    }
+
+    for(let i=0;i<=720;i++){
+        theta=theta%360;
+        let x= data.voltage*Math.sqrt(2)*calculateSine(theta);
+        if(theta>=data.firingangle1 && theta<=180){
+            sineValues.push(x);
+        }else if(theta >= data.firingangle2 && theta<=360){
+            sineValues.push(x);
+        }else{
+            sineValues.push(0);
+        }
+        theta+=1;
+    }
+
+    const ctx = document.getElementById("waveform");
+    if(chart!==null) chart.destroy();
+    chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            datasets:[{
+                label:"AC voltage controller waveform",
+                lineTension:0.17,
+                backgroundColor:"rgba(255, 121, 121,1.0)",
+                borderColor:"#2E0249",
+                data:sineValues
+            }],
+            labels:labelValue,
+        },
+        options: {
+            elements:{
+                point:{
+                    radius:0
+                }
+            },
+            scales:{
+                y: {
+                    suggestedMin: -15,
+                    suggestedMax: 15
+                },
+                yAxes:{
+                    title:{
+                        display:true,
+                        text:"Voltage(V)",
+                        font: {
+                            size: 15
+                        }
+                    }
+                },
+                xAxes:{
+                    title:{
+                        display:true,
+                        text:"Angle(degrees)",
+                        font: {
+                            size: 15
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+    output.classList.remove("hide");
+    simulation.classList.add("hide");
+}
+
+function showCircuit(){
+    chart.destroy();
+    output.classList.add("hide");
+    simulation.classList.remove("hide");
+}
+

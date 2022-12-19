ed16bdd477cc1709535a8e7a0f9e31d203454e6a
suriyamu1 <suriya.malarmurugesh@gmail.com>
2022-12-19 11:17:39 +0530

    Completed pre-quiz page for experiment-3 and experiment-4

---
+++ Script/PreQuiz/preQuizQues_Expt3.js
@@ -0,0 +1,60 @@
+
+const quiz =[
+	{	//1
+		q:'In dc choppers, if T<sub>on</sub> is the on-period and f is the chopping frequency, then output voltage in terms of input voltage V<sub>s</sub is given by',
+		options:['V<sub>s</sub> * T<sub>on</sub> / f','V<sub>s</sub> * f / T<sub>on</sub>','V<sub>s</sub> / T<sub>on</sub> * f','V<sub>s</sub> * T<sub>on</sub> * f'],
+		answer:3
+	},
+	{	//2
+		q:'In dc choppers, the waveforms for input and output voltages are respectively',
+		options:['discontinuous, continuous','both continuous','both discontinuous','continuous, discontinuous'],
+		answer:3
+	},
+	{	//3
+		q:'A chopper is a',
+		options:['Time ratio controller','AC to DC Converter','High speed semiconductor switch','Equivalent of a rheostat'],
+		answer:2
+	},
+	{	//4
+		q:'The output voltage of a chopper (using BJT) can be controlled by varying the',
+		options:['duty cycle','firing angle','reactor position','extinction angle'],
+		answer:0
+	},
+	{	//5
+		q:'In a step-up chopper, when the duty cycle is increased the average value of the output voltage',
+		options:['increases','decreases','remains the same','cannot be determined'],
+		answer:0
+	}, 
+	{
+		//6
+		q:"For a buck-boost converter, if the duty cycle > 0.5 then",
+		options:["V<sub>o</sub> = V<sub>s</sub>","V<sub>o</sub> &lt; V<sub>s</sub>", "V<sub>o</sub> &gt; V<sub>s</sub>","cannot be determined"],
+		answer: 2
+	}, 
+	{
+		//7
+		q:"A step-down chopper is also called as a",
+		options: ["(A) first-quadrant chopper","(B) second-quadrant chopper","(C) type-A chopper","(D) Both (A) and (C)"],
+		answer: 3
+	}, 
+	{
+		//8
+		q:"If frequency modulation control scheme is used, ___ is varied.",
+		options: ["ON-time","Time period","Supply frequency","Supply voltage"],
+		answer: 1
+	}, 
+	{
+		//9
+		q:"In case of frequency modulation control scheme, ____ is kept constant",
+		options: ["Time period","ON-time","OFF-time","Either ON-time or OFF-time"],
+		answer: 3
+	}, 
+	{
+		//10
+		q:"A step-up chopper has an input voltage of 5 V and the average output voltage of 15 V. The duty cycle is",
+		options: ["1/3","2/3","2/5","3/5"],
+		answer: 1
+	}
+]
+
+
---
+++ Script/PreQuiz/preQuizQues_Expt4.js
@@ -0,0 +1,89 @@
+const quiz =[
+	{	//1
+		q:"The output of AC Voltage controller is",
+		options:[
+            "fixed voltage, fixed frequency",
+            "fixed voltage, variable frequency",
+            "variable voltage, fixed frequency",
+            "variable voltage, variable frequency"
+        ],
+		answer:2
+	}, 
+    {
+        //2
+        q:"Before semi-conductor technology were developed, ____ devices were used for AC voltage control applications",
+        options:[
+            "cycloconverters",
+            "vacuum tubes",
+            "tap changing transformers",
+            "synchronous motors"
+        ],
+        answer: 2
+    },
+    {
+        //3
+        q:"Choose the correct application of AC voltage controllers",
+        options: [
+            "Power generation",
+            "Electric heating",
+            "Conveyer belt motion",
+            "Power transmission"
+        ],
+        answer: 1
+    }, 
+    {
+        //4
+        q:"In the principle of phase control, control is achieved by",
+        options: [
+            "adjusting the firing angle of the devices",
+            "adjusting the number of on-cycles and off-cycles",
+            "adjusting the on-time of freewheeling device",
+            "None of the above"
+        ],
+        answer: 0
+    }, 
+    {
+        //5
+        q: "A single phase half-wave voltage controller consists of",
+        options: [
+            "one SCR parallel with one diode",
+            "one SCR anti-parallel with one diode",
+            "two SCRs in parallel",
+            "two SCRs in anti-parallel"
+        ],
+        answer: 1
+    }, 
+    {
+        //6
+        q: "A single phase voltage controller has input of 230V rms and a resistive load of 15&#8486;. 6 ON-cycles and 4 OFF-cycles. Determine the RMS output voltage.",
+        options: [
+            "189 V",
+            "260 V",
+            "156 V",
+            "178 V"
+        ],
+        answer: 3
+    }, 
+    {
+        //7
+        q: "A single phase voltage controller has input of 230V rms and a resistive load of 15&#8486;. 6 ON-cycles and 4 OFF-cycles. Determine the power delivered to the load.",
+        options: [
+            "1.93 kW",
+            "2.10 kW",
+            "0.51 kW",
+            "5.16 kW"
+        ],
+        answer: 1
+    },
+    {
+        //8
+        q:"What is the advantage of integral cycle control",
+        options: [
+            "zero-voltage switching",
+            "less harmonics",
+            "average load power control",
+            "none of the above"
+        ],
+        answer:1
+    }
+]

0293216d7eb56e5f4623c9de9925c788173078c9
Suriya Murugesan <suriya.malarmurugesh@gmail.com>
2023-03-12 22:56:36 +0530

    Worked on experiment-5 circuit design page

--- Script/Expt5/Expt5_circuitDesign.js
+++ Script/Expt5/Expt5_circuitDesign.js
@@ -1,8 +1,14 @@
-const voltSlider = document.getElementById("voltSlider");
-const resSlider = document.getElementById("resSlider");
+const inputVoltSlider = document.getElementById("inputVoltSlider");
+const commVoltSlider = document.getElementById("commVoltSlider");
+const gateVoltSlider = document.getElementById("gateVoltSlider");
+const gateResSlider = document.getElementById("gateResSlider");
+const loadResSlider =document.getElementById("loadResSlider");
 
-const voltSpan = document.getElementById("voltSpan");
-const resSpan = document.getElementById("resSpan");
+const inputVoltSpan = document.getElementById("inputVoltSpan");
+const commVoltSpan = document.getElementById("commVoltSpan");
+const gateVoltSpan = document.getElementById("gateVoltSpan");
+const gateResSpan = document.getElementById("gateResSpan");
+const loadResSpan = document.getElementById("loadResSpan");
 
 const scr1 = document.getElementById("scr1");
 const diode1 = document.getElementById("diode1");
@@ -15,26 +21,44 @@ const exp_div2 = document.getElementById("exp_div2");
 const crct_img_div = document.getElementById("crct_img_div");
 const wrong_img_div = document.getElementById("wrong_img_div");
 
-voltSlider.oninput = () => {
-    voltSpan.innerHTML = voltSlider.value + "V AC";
-    verification();
-}
-resSlider.oninput = function(){
-    resSpan.innerHTML = this.value + " &#8486;";
-    verification();
-}
 
+inputVoltSlider.oninput = () => {
+    inputVoltSpan.innerHTML = inputVoltSlider.value + "V DC";
+    verification();
+}
+commVoltSlider.oninput = () => {
+    commVoltSpan.innerHTML = commVoltSlider.value + "V DC";
+    verification();
+}
+gateVoltSlider.oninput = () => {
+    gateVoltSpan.innerHTML = gateVoltSlider.value + "V DC";
+    verification();
+}
+gateResSlider.oninput = () => {
+    gateResSpan.innerHTML = gateResSlider.value +" &#8486;";
+    verification();
+}
+loadResSlider.oninput = () => {
+    loadResSpan.innerHTML = loadResSlider.value +" &#8486;";
+    verification();
+}
 function verification() {
     // this is to avoid the display of the images if the display is enabled by the below code.
     wrong_img_div.style.display = "none";
     crct_img_div.style.display = "none";
     
 
-    let v_max = (voltSlider.value)*Math.sqrt(2);
-    let i_max = v_max / resSlider.value;
-    
-    //displaying peak voltage and max load current values
-    exp_div2.innerHTML = "<h3 class='glassmorphism'>The peak voltage (V<sub>max</sub>) = V<sub>rms</sub> x 1.414 = "+v_max.toFixed(2)+"V<br>The max load current (I<sub>L</sub>) = "+ v_max.toFixed(2)+"V / " + resSlider.value+"&#8486; = "+ i_max.toFixed(3)+"A </h3>";
+    let vinMax = inputVoltSlider.value;
+    let i_max = vinMax / loadResSlider.value;
+    let i_gate = gateVoltSlider.value / gateResSlider.value;
+    localStorage.setItem("inputVolt",vinMax);
+    localStorage.setItem("loadCurrent",i_max);
+
+    //displaying output voltage, load current and load resistance values
+    exp_div2.innerHTML = 
+        "<h3 class='glassmorphism'>The output voltage (V<sub>in</sub>) = "+vinMax+"V<br>"+
+        "The load current (I<sub>L</sub>) = "+ vinMax+"V / " + loadResSlider.value+"&#8486; = "+ i_max.toFixed(3)+"A<br>" +
+        "The gate current (I<sub>g</sub>) = V<sub>g</sub>/R<sub>g</sub> = "+ gateVoltSlider.value +"/"+gateResSlider.value+" = "+ i_gate.toFixed(3)+"A</h3>";
 
     // alert message for choosing proper LOAD RESISTANCE
     if(i_max > 6 && i_max < 10) {
@@ -56,15 +80,19 @@ function verification() {
     }
 
     // VERIFICATION FOR SCR
-    if(verify_PIV("SCR",scr1.value, v_max)==false)   return;
-    if(verify_i_max("SCR",scr1.value, i_max)==false) return;
+    if(verify_PIV("Main SCR",scr1.value, vinMax)==false)   return;
+    if(verify_i_max("Main SCR",scr1.value, i_max)==false) return;
 
     exp_div2.innerHTML += "<h3 class='ok_green'>You have chosen the SCR correctly!!</h3>";
 
+    // VERIFICATION OF MAIN SCR GATE CURRENT
+    if (verify_i_gate("Main SCR",scr1.value, i_gate)==false)  return;
+    exp_div2.innerHTML += "<h3 class='ok_green'>You have designed the firing circuit correctly!!</h3>";
+    
     // CHOOSING DIODE (Optional)
     if(diode1.value != "Choose...") {
         //But if the user has chosen it, it should be chosen correctly
-        if(verify_PIV("Diode",diode1.value, v_max)==false)   return;
+        if(verify_PIV("Diode",diode1.value, vinMax)==false)   return;
         if(verify_i_max("Diode",diode1.value, i_max)==false) return;
         exp_div2.innerHTML += "<h3 class='ok_green'>You have chosen the Diode correctly!!</h3>";
     }
@@ -96,8 +124,8 @@ function verification() {
         wrong_img_div.style.display = "block";
         return;
     }
-    if(rated_v(voltmeter.value) < v_max) {
-        exp_div2.innerHTML += "<h3 class='alert_red'>The voltage rating of the chosen voltmeter ("+rated_v(voltmeter.value)+"V) is lesser than the peak output voltage ("+ v_max.toFixed(2)+"V). Hence this voltmeter cannot be chosen.</h3>";
+    if(rated_v(voltmeter.value) < vinMax) {
+        exp_div2.innerHTML += "<h3 class='alert_red'>The voltage rating of the chosen voltmeter ("+rated_v(voltmeter.value)+"V) is lesser than the peak output voltage ("+ vinMax.toFixed(2)+"V). Hence this voltmeter cannot be chosen.</h3>";
         wrong_img_div.style.display = "block";
         return;
     }
@@ -129,9 +157,9 @@ function get_i_max(component) {
     return -20;
 }
 
-function verify_PIV(name, component, v_max) {
-    if(get_PIV(component) < v_max) {
-        exp_div2.innerHTML += "<h3 class='alert_red'>The PIV of "+ name + " ("+ component +") is only "+get_PIV(component)+"V. Hence "+name+" cannot withstand the peak input voltage when it is reverse biased ("+v_max.toFixed(2)+"V)</h3>";
+function verify_PIV(name, component, vinMax) {
+    if(get_PIV(component) < vinMax) {
+        exp_div2.innerHTML += "<h3 class='alert_red'>The PIV of "+ name + " ("+ component +") is only "+get_PIV(component)+"V. Hence "+name+" cannot withstand the input voltage ("+vinMax+"V)</h3>";
         wrong_img_div.style.display = "block";
         return false;
     }
@@ -145,6 +173,30 @@ function verify_i_max(name, component, i_max) {
         return false;
     }
     return true;
+}
+
+function verify_i_gate(name, component, i_gate) {
+    console.log("max gate current: "+maxGateCurrent(component)+" "+component);
+    if(i_gate==0) {
+        exp_div2.innerHTML += "<h3 class='alert_red'>Gate current cannot be zero!!</h3>";
+        return false;
+    }
+    if(maxGateCurrent(component) < i_gate) {
+        exp_div2.innerHTML += "<h3 class='alert_red'>The maximum gate current of "+ name + " ("+ component +") is only "+maxGateCurrent(component)+"A. Hence "+name+" cannot withstand the gate current ("+i_gate.toFixed(3)+"A)"+
+        "<br>(Hint: Increase gate resistance or decrease gate voltage)</h3>";
+        wrong_img_div.style.display = "block";
+        return false;
+    }
+    return true;
+}
+
+function maxGateCurrent(scrName) {
+    if(scrName=="S4S1" || scrName=="S6004V")
+        return 1;
+    if(scrName=="H2P4M")
+        return 0.2;
+    if(scrName=="SK006L")
+        return 2;
 }
 
 function rated_i(ammeter_name) {
